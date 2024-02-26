93d207bc0b309fe9ac9d748046b4cd73d91889bb
FilipK19 <fkukic@student.unipu.hr>
2024-02-26 20:13:15 +0100

    back button

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ templates = Jinja2Templates(directory="templates")
 #Detalji za token
 SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 60
 
 #MongoDB baza
 MONGO_URI = "mongodb+srv://admin:admin123@cluster0.1cbo1.mongodb.net/"
